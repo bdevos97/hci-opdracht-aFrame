23987e7531dd2d8b09f4c86a2cc6c441b6926a8a
bdevos97 <106430657+bdevos97@users.noreply.github.com>
2022-06-04 19:15:41 +0200

    Add files via upload
    Erg irritant dat je die files niet meer kan verwijderen maar dan weer een hele nieuwe moet maken en uploaden

---
+++ 04Animation/scripts/main.js
@@ -0,0 +1,14 @@
+
+
+// // changing individual properties with code and using setInterval
+ var rotationSpeed = 0.002;
+ var myOtherBox = document.getElementById('myOtherBox');
+
+function spin(){
+	myOtherBox.object3D.rotation.x += -rotationSpeed;
+	myOtherBox.object3D.rotation.y += rotationSpeed;
+	myOtherBox.object3D.rotation.z += rotationSpeed/2;
+	console.log(myOtherBox.object3D.rotation.x);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
